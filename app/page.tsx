99dad342a288e3ab5b49107be52fd8657853a5f8
Aadarsha Khatri <adarskhatri1@gmail.com>
2025-01-14 16:30:48 +0545

    Meta Changed

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,11 @@ export default function Home() {
     <>
 
       <Head>
-          <meta property="og:url" content="https://dtoutagency.vercel.app/"></meta>
-        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://dtoutagency.vercel.app/"></meta>
+        <meta property="og:type" content="Website"></meta>
         <meta property="og:title" content="Dtout Agency - Book Now"></meta>
         <meta property="og:description" content="At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies."></meta>
-        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=aCTGi3yX-8Rw5Ek6e-Bg9Y1EuESB4bq_57NM4LnBBT4&height=500&width=500&expires=33272846886"></meta>
-
-
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta property="twitter:domain" content="dtoutagency.vercel.app"></meta>
-        <meta property="twitter:url" content="https://dtoutagency.vercel.app/"></meta>
-        <meta name="twitter:title" content="Dtout Agency - Book Now"></meta>
-        <meta name="twitter:description" content="At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies."></meta>
-        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=aCTGi3yX-8Rw5Ek6e-Bg9Y1EuESB4bq_57NM4LnBBT4&height=500&width=500&expires=33272846886"></meta>
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=aCTGi3yX-8Rw5Ek6e-Bg9Y1EuESB4bq_57NM4LnBBT4&height=500&width=500&expires=33272846886"></meta>       
       </Head>
 
 
