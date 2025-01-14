4f0c340d27d7462dcb6b1e0806c05eb1260b913e
Aadarsha Khatri <adarskhatri1@gmail.com>
2025-01-14 16:47:40 +0545

    tags added into metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 
 import { Poppins} from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 
 
 
@@ -13,7 +13,22 @@ export const metadata = {
   description: "At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies.",
   icons: {
     icon: "/icon.png",
-  }
+  },
+  openGraph: {
+    url: "https://dtoutagency.vercel.app/",
+    type: "website",
+    title: "Dtout Agency - Book Now",
+    description: "At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies.",
+    image: "https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=iTkb6y96WvB5Eswxfy33QFgDZck-HnV6Uir_Z7eaAzg&height=500&width=500&expires=33272851741",
+  },
+  twitter: {
+    card: "summary_large_image",
+    domain: "dtoutagency.vercel.app",
+    url: "https://dtoutagency.vercel.app/",
+    title: "Dtout Agency - Book Now",
+    description: "At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies.",
+    image: "https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=iTkb6y96WvB5Eswxfy33QFgDZck-HnV6Uir_Z7eaAzg&height=500&width=500&expires=33272851741",
+  },
 };
 
 export default function RootLayout({
@@ -23,26 +38,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <Head>
-      
-      <title>Dtout Agency - Book Now</title>
-      <meta name="description" content="At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies." />
-
-     
-      <meta property="og:url" content="https://dtoutagency.vercel.app/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Dtout Agency - Book Now" />
-      <meta property="og:description" content="At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies." />
-      <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=iTkb6y96WvB5Eswxfy33QFgDZck-HnV6Uir_Z7eaAzg&height=500&width=500&expires=33272851741" />
-
-  
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="dtoutagency.vercel.app" />
-      <meta property="twitter:url" content="https://dtoutagency.vercel.app/" />
-      <meta name="twitter:title" content="Dtout Agency - Book Now" />
-      <meta name="twitter:description" content="At Dtout Agency, we are dedicated to helping dental clinics achieve exceptional growth through tailored digital marketing strategies." />
-      <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/8bfed5a0-5f95-4d4d-b678-d10bca468963.png?token=iTkb6y96WvB5Eswxfy33QFgDZck-HnV6Uir_Z7eaAzg&height=500&width=500&expires=33272851741" />
-    </Head>
       <body
         className={`${pop.className} bg-white antialiased`}
       >
